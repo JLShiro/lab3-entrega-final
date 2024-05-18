10b542e2237fd7885fd0af232ba3b708709cffaf
José Liao <tlh.tf2_@hotmail.com>
2024-05-18 10:17:59 -0500

    Primer Commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+    return (
+        <header>
+            <img src="/path-to-your-logo.png" alt="Logo" />
+        </header>
+    );
+}
+
+export default Header;
